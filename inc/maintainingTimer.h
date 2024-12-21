646b184c6595f6da27502ef58c43c71739a92950
mKopichev <mmkopychev@etu.ru>
2024-12-21 11:38:27 +0300

    initial commit

--- inc/maintainingTimer.h
+++ inc/maintainingTimer.h
@@ -3,7 +3,7 @@
 
 // MAINTAINING_PERIOD - period for data collecting and peripheral modules switching
 // TEMP_SETPOINT - if temperature below that value, heating lamp turns on
-// TEMP_SETPOINT_DELTA - if temperature over TEMP_SETPOIT+TEMP_SETPOINT_DELTA, cooling fan turn on
+// TEMP_SETPOINT_DELTA - if temperature over TEMP_SETPOIT+TEMP_SETPOINT_DELTA, cooling fan turns on
 // LIGHT SETPOINT - if photoresistor return value less, fitolamp turns on
 // MOISTURE_SETPOINT  -if maisture sensor return value less, plant watering should be turned on
 
@@ -11,9 +11,9 @@
 
 #define MAINTAINIG_PERIOD   600L  // sec
 #define TEMP_SETPOINT       22.5F // oC
-#define TEMP_SETPOINT_DELTA 5.0F  // oC
+#define TEMP_SETPOINT_DELTA 3.5F  // oC
 #define LIGHT_SETPOINT      0.6F  // %
-#define MOISTURE_SETPOINT   0.8F  // %
+#define MOISTURE_SETPOINT   0.4F  // %
 
 void timerInit(void);
 void climateMaining(void);
